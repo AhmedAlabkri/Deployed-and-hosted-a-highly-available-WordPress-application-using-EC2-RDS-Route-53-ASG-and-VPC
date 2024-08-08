8b850eaee50f89a00e70415f087011a1a9957806
AhmedAlabkri <123089485+AhmedAlabkri@users.noreply.github.com>
2024-08-08 08:54:04 +0300

    Create launch_template.sh

---
+++ scripts/launch_template.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update the software packages on the EC2 instance
+sudo yum update -y
+
+# Install and start Apache web server
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+# Install PHP 8 and necessary extensions for WordPress
+sudo dnf install -y php php-cli php-cgi php-curl php-mbstring php-gd php-mysqlnd php-gettext php-json php-xml php-fpm php-intl php-zip php-bcmath php-ctype php-fileinfo php-openssl php-pdo php-tokenizer
+
+# Install MySQL Community Repository
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+sudo dnf install -y mysql80-community-release-el9-1.noarch.rpm
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf repolist enabled | grep "mysql.*-community.*"
+sudo dnf install -y mysql-community-server
+
+# Start and enable MySQL server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+# Set environment variable for EFS DNS name
+EFS_DNS_NAME=
+
+# Mount the EFS to the HTML directory
+echo "$EFS_DNS_NAME:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+mount -a
+
+# Set permissions
+chown apache:apache -R /var/www/html
+
+# Restart the webserver
+sudo service httpd restart
